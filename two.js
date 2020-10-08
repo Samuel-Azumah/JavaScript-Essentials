ee7e2f847091a6601e9e4095f541cc679148cfd6
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2020-10-08 01:15:36 -1200

    Add files via upload

---
+++ two.js
@@ -0,0 +1,12 @@
+
+let sammy = document.getElementByName('Cstyle');
+let azumah = document.getElementById('new');
+function mawuli()
+{
+    for (let one =0; i<sammy.length; one++){
+        let Cstyle=sammy[one].getAttribute('id');
+        let Cv=sammy[ one ].value;
+        azumah.style[ Cstyle ]= Cv;
+    }
+}
+document.getElementById('set').addEventListener('click',set);
