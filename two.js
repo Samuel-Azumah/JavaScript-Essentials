21b337863f33484027adc78a640e888064f88ea0
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2020-10-08 01:21:08 -1200

    Update two.js

--- two.js
+++ two.js
@@ -3,7 +3,7 @@ let sammy = document.getElementByName('Cstyle');
 let azumah = document.getElementById('new');
 function mawuli()
 {
-    for (let one =0; i<sammy.length; one++){
+    for (let one =0; one<sammy.length; one++){
         let Cstyle=sammy[one].getAttribute('id');
         let Cv=sammy[ one ].value;
         azumah.style[ Cstyle ]= Cv;
